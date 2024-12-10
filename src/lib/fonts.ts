67e48e4dd5d3336f2a7d713f89e89c30543e3e44
Yannick Remy <remyyannick@sfr.fr>
2024-12-10 18:49:55 +0100

    Remove unused fonts

--- src/lib/fonts.ts
+++ src/lib/fonts.ts
@@ -3,21 +3,6 @@ import localFont from "next/font/local";
 export const inter = localFont({
   src: [
     {
-      path: "../fonts/Inter/Inter-Thin.woff2",
-      weight: "100",
-      style: "normal",
-    },
-    {
-      path: "../fonts/Inter/Inter-ExtraLight.woff2",
-      weight: "200",
-      style: "normal",
-    },
-    {
-      path: "../fonts/Inter/Inter-Light.woff2",
-      weight: "300",
-      style: "normal",
-    },
-    {
       path: "../fonts/Inter/Inter-Regular.woff2",
       weight: "400",
       style: "normal",
@@ -32,21 +17,6 @@ export const inter = localFont({
       weight: "600",
       style: "normal",
     },
-    {
-      path: "../fonts/Inter/Inter-Bold.woff2",
-      weight: "700",
-      style: "normal",
-    },
-    {
-      path: "../fonts/Inter/Inter-ExtraBold.woff2",
-      weight: "800",
-      style: "normal",
-    },
-    {
-      path: "../fonts/Inter/Inter-Black.woff2",
-      weight: "900",
-      style: "normal",
-    },
   ],
   variable: "--font-inter",
 });
@@ -54,28 +24,8 @@ export const inter = localFont({
 export const interDisplay = localFont({
   src: [
     {
-      path: "../fonts/InterDisplay/InterDisplay-Thin.woff2",
-      weight: "100",
-      style: "normal",
-    },
-    {
-      path: "../fonts/InterDisplay/InterDisplay-ExtraLight.woff2",
-      weight: "200",
-      style: "normal",
-    },
-    {
-      path: "../fonts/InterDisplay/InterDisplay-Light.woff2",
-      weight: "300",
-      style: "normal",
-    },
-    {
       path: "../fonts/InterDisplay/InterDisplay-Regular.woff2",
       weight: "400",
-      style: "normal",
-    },
-    {
-      path: "../fonts/InterDisplay/InterDisplay-Medium.woff2",
-      weight: "500",
       style: "normal",
     },
     {
@@ -91,11 +41,6 @@ export const interDisplay = localFont({
     {
       path: "../fonts/InterDisplay/InterDisplay-ExtraBold.woff2",
       weight: "800",
-      style: "normal",
-    },
-    {
-      path: "../fonts/InterDisplay/InterDisplay-Bold.woff2",
-      weight: "900",
       style: "normal",
     },
   ],
