31dfd80105b4def40182b6e77614536077a0e80d
Yannick Remy <remyyannick@sfr.fr>
2024-12-10 18:36:22 +0100

    Update disallow rule

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -8,7 +8,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: "*",
         allow: "/",
-        disallow: ["/api/", "/contact/__tests__", "/favicon.ico", "/apple-icon.png", "/icon.svg"],
+        disallow: ["/api/", "/contact/__tests__"],
       },
     ],
     sitemap: `${baseUrl}/sitemap.xml`,
